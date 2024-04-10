e8ed13fd3c418823d5ffa63387a8a5eecdb6a367
HaiChao <896278956@qq.com>
2024-04-10 23:58:51 +0800

    修改app/ui/fonts导出错误

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { inter } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import Image from 'next/image';
 
 export default async function CustomersTable({
   customers,
@@ -13,7 +10,7 @@ export default async function CustomersTable({
 }) {
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+      <h1 className={`${inter.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
       <Search placeholder="Search customers..." />
