647658644f290f20a697e63f0bb49b2d81df1529
HaiChao <896278956@qq.com>
2024-04-11 12:07:49 +0800

    解决请求瀑布的问题

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,32 +9,44 @@ import { lusitana } from '@/app/ui/fonts';
 import { Card } from '../ui/dashboard/cards';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+  // 解决请求瀑布的问题
+  const revenue = fetchRevenue();
+  const latestInvoices = fetchLatestInvoices();
+  const total_data = fetchCardData();
+  const data = await Promise.all([revenue, latestInvoices, total_data]);
+  const revenueData = data[0];
+  const latestInvoicesData = data[1];
+  const totalData = data[2];
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Collected"
+          value={totalData.totalPaidInvoices}
+          type="collected"
+        />
+        <Card
+          title="Pending"
+          value={totalData.totalPendingInvoices}
+          type="pending"
+        />
+        <Card
+          title="Total Invoices"
+          value={totalData.numberOfInvoices}
+          type="invoices"
+        />
         <Card
           title="Total Customers"
-          value={numberOfCustomers}
+          value={totalData.numberOfCustomers}
           type="customers"
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <RevenueChart revenue={revenueData} />
+        <LatestInvoices latestInvoices={latestInvoicesData} />
       </div>
     </main>
   );
